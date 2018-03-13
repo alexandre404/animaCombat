5dcd55db894734fcc0d03f6879e93f6963a6ebfc
alexandre404 <dragnen404@gmail.com>
2018-03-13 21:46:50 +0100

    Fonction parade et esquive faites

--- animaCombat/Animap1/Animap1/Form1.cs
+++ animaCombat/Animap1/Animap1/Form1.cs
@@ -97,8 +97,6 @@ namespace Animap1
             Choix();
         }
 
-      
-
         private void cbVision_SelectedIndexChanged(object sender, EventArgs e)
         {
             Choix();
@@ -126,6 +124,18 @@ namespace Animap1
             if (rbAttaque.Checked)
             {
                 Res = Attaque();
+
+                this.txtResultat.Text = Res.ToString();
+            }
+            if (rbPare.Checked)
+            {
+                Res = Pare();
+
+                this.txtResultat.Text = Res.ToString();
+            }
+            if (rbEsquive.Checked)
+            {
+                Res = Esquive();
 
                 this.txtResultat.Text = Res.ToString();
             }
@@ -205,10 +215,173 @@ namespace Animap1
             {
                 Res = Res - 10;
             }
+            if (cbPosition.Text == "Paralisie totale")
+            {
+                Res = Res - 200;
+            }
+            if (cbPosition.Text == "Paralysie partielle")
+            {
+                Res = Res - 80;
+            }
+            if (cbPosition.Text == "Paralysie légère")
+            {
+                Res = Res - 20;
+            }
+            return Res;
+
+        }
+
+        private int Pare()
+        {
+            int Res = 0;
+        Res = +Convert.ToInt32(txtAtt.Text);
+            if (CbCharger.Checked)
+            {
+                Res = Res-10;
+            }
+            if(CbDegainer.Checked)
+            {
+                Res = Res-25;
+            }
+            if(CbRenverse.Checked)
+            {
+                Res = Res-30;
+            }
+            if(CbSurpris.Checked)
+            {
+                Res = -90;
+            }
+            if (CbCombattant.Checked)
+            {
+                if (CbArme.Text == "Arme grande")
+                {
+                    Res = Res - 40;
+                }
+
+            }
+            if(CbEnJoue.Checked)
+            {
+                Res = Res - 120;
+            }
+            if(cbTaille.Text == "minuscule")
+            {
+                Res = Res - 10;
+            }
+            if (cbVision.Text == "Cécité partielle")
+            {
+                Res = Res - 30;
+            }
+            if (cbVision.Text == "Cécité totale")
+            {
+                Res = Res - 80;
+            }
+            if (cbVol.Text == "Perso en lévitation")
+            {
+                Res = Res - 20;
+            }
+            if (cbVol.Text == "Perso en vol (7-14)")
+            {
+                Res = Res + 10;
+            }
+            if (cbVol.Text == "Perso en vol (15+)")
+            {
+                Res = Res + 10;
+            }
+            if (cbPosition.Text == "dans le dos du personnage")
+            {
+                Res = Res - 80;
+            }
+            if (cbPosition.Text == "dans le flanc du personnage")
+            {
+                Res = Res - 30;
+            }
+            if (cbPosition.Text == "Paralisie totale")
+            {
+                Res = Res - 200;
+            }
+            if (cbPosition.Text == "Paralysie partielle")
+            {
+                Res = Res - 80;
+            }
+            if (cbPosition.Text == "Paralysie légère")
+            {
+                Res = Res - 20;
+            }
             return Res;
             
         }
 
+        private int Esquive()
+        {
+            int Res = 0;
+            Res = +Convert.ToInt32(txtAtt.Text);
+            if (CbCharger.Checked)
+            {
+                Res = Res - 20;
+            }
+            if (CbDegainer.Checked)
+            {
+                Res = Res - 25;
+            }
+            if (CbRenverse.Checked)
+            {
+                Res = Res - 30;
+            }
+            if (CbSurpris.Checked)
+            {
+                Res = -90;
+            }
+            if (CbCombattant.Checked)
+            {
+                    Res = Res - 40;
+            }
+            if (CbEnJoue.Checked)
+            {
+                Res = Res - 120;
+            }
+            if (cbVision.Text == "Cécité partielle")
+            {
+                Res = Res - 15;
+            }
+            if (cbVision.Text == "Cécité totale")
+            {
+                Res = Res - 80;
+            }
+            if (cbVol.Text == "Perso en lévitation")
+            {
+                Res = Res - 40;
+            }
+            if (cbVol.Text == "Perso en vol (7-14)")
+            {
+                Res = Res + 10;
+            }
+            if (cbVol.Text == "Perso en vol (15+)")
+            {
+                Res = Res + 10;
+            }
+            if (cbPosition.Text == "dans le dos du personnage")
+            {
+                Res = Res - 80;
+            }
+            if (cbPosition.Text == "dans le flanc du personnage")
+            {
+                Res = Res - 30;
+            }
+            if (cbPosition.Text == "Paralisie totale")
+            {
+                Res = Res - 100;
+            }
+            if (cbPosition.Text == "Paralysie partielle")
+            {
+                Res = Res - 30;
+            }
+            if (cbPosition.Text == "Paralysie légère")
+            {
+                Res = Res - 40;
+            }
+            return Res;
+
+        }
 
     }
 }
